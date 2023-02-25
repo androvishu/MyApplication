52c913ad9fb448c4188db30254d0f12d735991e4
Vishal <vishalrawat979@gmail.com>
2023-02-25 19:58:28 +0530

    Initial commit

---
+++ App.js
@@ -0,0 +1,18 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const App = () => {
+  return (
+    <View style={styles.mainContainer}>
+      <Text>App</Text>
+    </View>
+  )
+}
+
+export default App;
+
+const styles = StyleSheet.create({
+  mainContainer: {
+    backgroundColor: "gray";
+  }
+})
